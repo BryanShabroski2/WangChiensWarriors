83340831c273591d3434ebcd357ceb511e231571
bryan <bryanshabroski@gmail.com>
2025-04-23 20:00:54 -0400

    Added registration

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
@@ -38,6 +38,80 @@ def login():
     else:
         return render_template('index.html', error='Invalid email or password.')
 
+@@app.route('/register', methods=['GET'])
+def register_form():
+    return render_template('register.html')
+
+# User registration process form
+@app.route('/register', methods=['POST'])
+def register():
+    #Get email password and role from form
+    email = request.form['email']
+    password = request.form['password']
+    confirm_password = request.form['confirm_password']
+    role = request.form['role']
+
+    #Make sure confirmed password matches
+    if password != confirm_password:
+        return render_template('register.html', error='Passwords do not match.')
+    #Generate a hashed password
+    hashed_password = generate_password_hash(password)
+    #Connect to the database and connect the cursor
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+
+    #Make sure user does not already exist
+    cursor.execute('SELECT email FROM users WHERE email = ?', (email,))
+    if cursor.fetchone():
+        connection.close()
+        return render_template('register.html', error='Email already registered.')
+
+    #Insert into users table
+    cursor.execute(
+        'INSERT INTO users (email, hashed_password) VALUES (?, ?)',
+        (email, hashed_password)
+    )
+
+    #Based on role we insert that form information into the database
+    if role == 'buyer':
+        business_name = request.form['business_name']
+        buyer_address_id = request.form['buyer_address_id']
+
+        cursor.execute(
+            'INSERT INTO buyers (email, business_name, buyer_address_id) VALUES (?, ?, ?)',
+            (email, business_name, buyer_address_id)
+        )
+
+    elif role == 'seller':
+        business_name = request.form['business_name']
+        business_address_id = request.form['business_address_id']
+        bank_routing_number = request.form['bank_routing_number']
+        bank_account_number = request.form['bank_account_number']
+
+        cursor.execute(
+            'INSERT INTO sellers (email, business_name, Business_Address_ID, bank_routing_number, bank_account_number, balance) VALUES (?, ?, ?, ?, ?, ?)',
+            (email, business_name, business_address_id, bank_routing_number, bank_account_number, 0.0)
+        )
+
+    elif role == 'helpdesk':
+        position = request.form['position']
+
+        cursor.execute(
+            'INSERT INTO helpdesk (email, Position) VALUES (?, ?)',
+            (email, position)
+        )
+    #Close
+    connection.commit()
+    connection.close()
+
+    #Automatically log in user.
+    session['user'] = {
+        'email': email,
+        'role': role
+    }
+
+    #redirect to mainpage
+    return redirect(url_for('mainpage'))
 
 
 
